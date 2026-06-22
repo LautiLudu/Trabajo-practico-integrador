eb5d5336203a7eb29b6ea6cc4b7855da346aec5e
LautiLudu <lautaroluduenia@gmail.com>
2026-06-21 21:37:58 -0300

    Update juego.h

--- headers/juego.h
+++ headers/juego.h
@@ -1,1 +1,16 @@
+#ifndef JUEGO_H_INCLUDED
+#define JUEGO_H_INCLUDED
+#include <iostream>
 
+using namespace std;
+
+/// declaracion de funciones
+void jugar();
+
+void generarDados(int dados[], int cantDados);
+void mostrarDados(int dados[], int cantDados);
+
+int calcularPts(int dados[],int cantDados, int viento1, int viento2);
+int contBloqueados(int dados[], int cantDados, int viento1, int viento2);
+
+#endif // JUEGO_H_INCLUDED
