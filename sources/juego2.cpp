524b9af43d380767d6177c92574c47981cd683de
LautiLudu <lautaroluduenia@gmail.com>
2026-06-24 16:12:29 -0300

    Create juego2.cpp

---
+++ sources/juego2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "juego.h"
+#include <clocale>
+
+using namespace std;
+
+void jugar2(string &nombreRec, int &puntajeRec){
+    bool partidaDe2 = true;
+
+    int ptsJugador1 = 0;
+    int ptsJugador2 = 0;
+
+    string jugador1;
+    string jugador2;
+
+    system("cls");
+
+    cout << "Ingrese el nombre del jugador 1: ";
+    cin >> jugador1;
+
+    cout << "Ingrese el nombre del jugador 2: ";
+    cin >> jugador2;
+
+    cout << "Presione cualquier tecla para empezar" << endl;
+    system("pause > nul");
+
+    ptsJugador1 = jugar(jugador1, nombreRec, puntajeRec);
+
+    cout << "Presione cualquier tecla para seguir con las rondas de " << jugador2 << endl;
+    system("pause > nul");
+
+    ptsJugador2 = jugar(jugador2, nombreRec, puntajeRec);
+
+    separacion();
+
+    cout << "Resultados finales: " << endl;
+    cout << jugador1 << ": " << ptsJugador1 << " puntos" << endl;
+    cout << jugador2 << ": " << ptsJugador2 << " puntos" << endl;
+
+    if(ptsJugador1 > ptsJugador2){
+    cout << "Gana " << jugador1 << "!" << endl;
+
+    }else{
+        if(ptsJugador2 > ptsJugador1){
+            cout << "Gana " << jugador2 << endl;
+
+        }else{
+        cout << "Empate" << endl;
+        }
+
+}
+    cout << "Presione cualquier tecla para volver al menu";
+    system("pause > nul");
+}
