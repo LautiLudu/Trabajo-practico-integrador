764e54fb8ad49b7ca58a1a02b8023c990c5c834f
LautiLudu <lautaroluduenia@gmail.com>
2026-06-24 16:11:41 -0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -5,6 +5,11 @@ using namespace std;
 
 int main()
 {
+    ///configuracion de texto para los caracteres en español
+    setlocale(LC_ALL, "Spanish");
+
+    srand(time(NULL));
+
     menuPrincipal();
 
     return 0;
